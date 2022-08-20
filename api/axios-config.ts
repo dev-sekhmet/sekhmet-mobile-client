9a3e95ee1855c46a0527ab36b1ef02c868a07b3a
gtemate <gaetan@temate.fr>
2022-08-20 17:48:15 +0200

    feature[SMC-19] save

--- api/axios-config.ts
+++ api/axios-config.ts
@@ -4,6 +4,6 @@ import {AUTH_TOKEN_KEY} from "./authentification/authentication.reducer";
 
 const TIMEOUT = 60 * 1000;
 export const axiosInstance = axios.create({
-    baseURL: 'http://8c77-2a01-e34-ec13-f440-b53c-5e59-b4a2-96ef.ngrok.io/api',
+    baseURL: 'http://6491-2a01-e34-ec13-f440-f596-d486-661f-6fdc.ngrok.io/api',
     timeout: TIMEOUT
 });
